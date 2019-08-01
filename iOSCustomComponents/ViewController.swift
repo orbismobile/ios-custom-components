b25a1b917f7622b64bf4d2e3a14daf01155dd478
Carlos Leonardo Camilo Vargas Huamán <carlosleonardovargashuaman@gmail.com>
2019-08-01 09:48:32 -0500

    Initial Commit

---
+++ iOSCustomComponents/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  iOSCustomComponents
+//
+//  Created by Carlos Leonardo Camilo Vargas Huamán on 8/1/19.
+//  Copyright © 2019 El Comercio. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
